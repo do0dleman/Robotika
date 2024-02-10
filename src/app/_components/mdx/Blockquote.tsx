ccae41f26879ea6d1cb1edef15e2e0385dfdc6ef
do0dleman <97869897+do0dleman@users.noreply.github.com>
2024-02-10 12:03:58 +0200

    minor style change

--- src/app/_components/mdx/Blockquote.tsx
+++ src/app/_components/mdx/Blockquote.tsx
@@ -1,7 +1,7 @@
 import { CgInfo } from "react-icons/cg";
 
 export default function Blockquote({ children }: { children?: React.ReactNode }) {
-    return <blockquote className=" relative mdx-blockquote rounded-2xl italic pl-8 pb-6 py-4 pr-4 
+    return <blockquote className=" relative mdx-blockquote rounded-2xl italic pl-6 pb-6 py-4 pr-4 
     [&>p]:mb-0 mb-4 overflow-hidden before:absolute before:h-full 
     before:w-[.3em] before:bg-info before:top-0 before:left-0
     after:absolute after:w-full after:h-full after:bg-info 
