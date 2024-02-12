8d9ea7f61c45b5ccece39f947f32cf434ac0a1f6
do0dleman <97869897+do0dleman@users.noreply.github.com>
2024-02-12 10:39:43 +0200

    fixed bitmap drawer pre block

--- src/app/_components/mdx/Pre.tsx
+++ src/app/_components/mdx/Pre.tsx
@@ -7,13 +7,14 @@ type PreProps = {
 }
 
 export default function Pre({ children, raw, lang }: PreProps) {
+    console.log(raw)
     return (
         <div className="mb-8 pb-4 bg-bgSecondary rounded-2xl">
-            <div className="pt-2 px-4 pb-2 text-2xl border-primary bg-bgTertiary 
+            {raw ? <div className="pt-2 px-4 pb-2 text-2xl border-primary bg-bgTertiary 
             flex justify-between items-center rounded-t-2xl">
                 <h3 className="text-h3 font-bold">{lang}</h3>
                 <CopyButton copyText={raw} completeText="Copied" />
-            </div>
+            </div> : <></>}
             <div className="px-4 pt-4">
                 <pre className="mdx-pre overflow-y-auto max-h-[50vh] block [&>code]:bg-bgSecondary">
                     {children}
