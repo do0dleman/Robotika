d5a126242cc28b0078dae920586f1e46d01cd7c5
do0dleman <97869897+do0dleman@users.noreply.github.com>
2024-02-12 06:17:02 +0200

    added copied  message

--- src/app/_components/CopyButton.tsx
+++ src/app/_components/CopyButton.tsx
@@ -1,29 +1,49 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaRegCopy } from "react-icons/fa";
 interface CopyButtonProps extends React.ComponentProps<'button'> {
-    text?: string
+    copyText?: string
+    completeText: string
 }
-function CopyButton({ text, ...rest }: CopyButtonProps) {
+function CopyButton({ copyText, completeText, ...rest }: CopyButtonProps) {
     const [isCopied, setCopied] = useState(false)
 
     function HandleCopyButtonClick() {
-        if (!text || isCopied) {
+        if (!copyText || isCopied) {
             return
         }
-        void navigator.clipboard.writeText(text)
+        void navigator.clipboard.writeText(copyText)
         setCopied(true)
     }
+
+    useEffect(() => {
+        if (isCopied === false) {
+            return
+        }
+
+        const timeoutId = setTimeout(() => {
+            setCopied(false)
+        }, 1000)
+
+        return () => clearTimeout(timeoutId)
+    }, [isCopied])
     return (
         <button
             className="hover:bg-bgSecondary active:bg-bgBase 
             transition-colors duration-300 rounded-lg p-2
-            disabled:bg-transparent disabled:text-inactive"
+            disabled:bg-transparent disabled:text-inactive relative"
             onClick={HandleCopyButtonClick}
-            disabled={isCopied}
+            // disabled={isCopied}
             {...rest}>
             <FaRegCopy />
+            <div className={`absolute left-1/2 -bottom-1 
+            translate-y-full -translate-x-1/2 text-base
+            bg-bgBase px-1.5 rounded-md transition-opacity
+            duration-300 shadow-inner shadow-bgSecondary
+            ${isCopied ? "opacity-100" : "opacity-0"}`}>
+                {completeText}
+            </div>
         </button>
     )
 }
--- src/app/_components/mdx/Pre.tsx
+++ src/app/_components/mdx/Pre.tsx
@@ -3,16 +3,16 @@ import CopyButton from "../CopyButton";
 type PreProps = {
     children?: React.ReactNode,
     raw?: string,
-    lang?: string
+    lang?: string,
 }
 
 export default function Pre({ children, raw, lang }: PreProps) {
     return (
-        <div className="mb-8 pb-4 bg-bgSecondary rounded-2xl overflow-hidden">
+        <div className="mb-8 pb-4 bg-bgSecondary rounded-2xl">
             <div className="pt-2 px-4 pb-2 text-2xl border-primary bg-bgTertiary 
-            flex justify-between items-center">
+            flex justify-between items-center rounded-t-2xl">
                 <h3 className="text-h3">{lang}</h3>
-                <CopyButton text={raw} />
+                <CopyButton copyText={raw} completeText="Copied" />
             </div>
             <div className="px-4 pt-4">
                 <pre className="mdx-pre overflow-y-auto max-h-[50vh] block [&>code]:bg-bgSecondary">
