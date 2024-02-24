b538e69e1a0e6956802a8e2b7935215236f3af75
do0dleman <97869897+do0dleman@users.noreply.github.com>
2024-02-24 10:37:40 +0200

    foxed task card animation on hover

--- src/app/_components/TaskCard.tsx
+++ src/app/_components/TaskCard.tsx
@@ -6,7 +6,7 @@ import P from "./mdx/P"
 function TaskCard({ taskInfo }: { taskInfo: TaskModel }) {
     return (
         <Link href={`tasks/${taskInfo.slug}`} className="border border-primary px-4 rounded-2xl mb-4 max-w-3xl block
-        text-primary hover:text-primary hover:no-underline hover:-translate-y-1.5">
+        text-primary hover:text-primary hover:no-underline hover:-translate-y-1.5 transition-transform duration-300">
             <H2>{taskInfo.title}</H2>
             <P>{taskInfo.description}</P>
         </Link>
