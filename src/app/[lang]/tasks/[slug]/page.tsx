4d40b7c482fa92be96370232828898c1e8259f56
do0dleman <nikita.obrazcovs@gmail.com>
2025-08-31 08:57:41 +0300

    replaced broken plugin

--- src/app/[lang]/tasks/[slug]/page.tsx
+++ src/app/[lang]/tasks/[slug]/page.tsx
@@ -1,35 +1,44 @@
-import getConfig from "next/config"
-import fs from "fs/promises"
-import path from "path"
-import matter from "gray-matter"
-import MDXRemoteWrapper from "~/app/_components/mdx/MDXRemoteWrapper"
-import Link from "next/link"
-import { Locale } from "i18n.config"
+import getConfig from "next/config";
+import fs from "fs/promises";
+import path from "path";
+import matter from "gray-matter";
+import MDXRemoteWrapper from "~/app/_components/mdx/MDXRemoteWrapper";
+import Link from "next/link";
+import { type Locale } from "i18n.config";
 
-async function Task({ params }: { params: { lang: Locale, slug: string } }) {
-    const { publicRuntimeConfig } = getConfig()
-    const TASK_DIR = publicRuntimeConfig.taskDir
+async function Task({ params }: { params: { lang: Locale; slug: string } }) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { publicRuntimeConfig } = getConfig();
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  const TASK_DIR = publicRuntimeConfig.taskDir as string;
 
-    let file
-    try {
-        file = await fs.readFile(path.join(process.cwd(), TASK_DIR, params.slug + `-${params.lang}` + '.mdx'), {
-            encoding: "utf-8"
-        })
-    } catch { }
+  let file;
+  try {
+    file = await fs.readFile(
+      path.join(
+        process.cwd(),
+        TASK_DIR,
+        params.slug + `-${params.lang}` + ".mdx",
+      ),
+      {
+        encoding: "utf-8",
+      },
+    );
+  } catch {}
 
-    if (file === undefined) {
-        return (
-            <div className="h-screen flex flex-col items-center justify-center">
-                <h1 className="text-4xl mb-2">{`Error! Task "${params.slug}" not found.`}</h1>
-                <Link className="underline" href='/'>Go to the home page</Link>
-            </div>
-        )
-    }
-
-    const fileData = matter(file)
-
+  if (file === undefined) {
     return (
-        <MDXRemoteWrapper source={fileData.content} />
-    )
+      <div className="flex h-screen flex-col items-center justify-center">
+        <h1 className="mb-2 text-4xl">{`Error! Task "${params.slug}" not found.`}</h1>
+        <Link className="underline" href="/">
+          Go to the home page
+        </Link>
+      </div>
+    );
+  }
+
+  const fileData = matter(file);
+
+  return <MDXRemoteWrapper source={fileData.content} />;
 }
-export default Task
+export default Task;
--- src/app/_components/mdx/MDXRemoteWrapper.tsx
+++ src/app/_components/mdx/MDXRemoteWrapper.tsx
@@ -1,70 +1,72 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
-import Blockquote from '~/app/_components/mdx/Blockquote'
-import H1 from '~/app/_components/mdx/H1'
-import H2 from '~/app/_components/mdx/H2'
-import H3 from '~/app/_components/mdx/H3'
-import P from '~/app/_components/mdx/P'
-import Strong from '~/app/_components/mdx/Strong'
-import Ul from '~/app/_components/mdx/Ul'
-import rehypeHighlight from 'rehype-highlight';
-import Image from "next/image"
-import remarkMdxImages from 'remark-mdx-images'
-import remarkGfm from 'remark-gfm'
-import Pre from "~/app/_components/mdx/Pre"
-import Code from "~/app/_components/mdx/Code"
-import { visit } from 'unist-util-visit'
-import A from './A'
+import { MDXRemote } from "next-mdx-remote/rsc";
+import Blockquote from "~/app/_components/mdx/Blockquote";
+import H1 from "~/app/_components/mdx/H1";
+import H2 from "~/app/_components/mdx/H2";
+import H3 from "~/app/_components/mdx/H3";
+import P from "~/app/_components/mdx/P";
+import Strong from "~/app/_components/mdx/Strong";
+import Ul from "~/app/_components/mdx/Ul";
+import rehypeHighlight from "rehype-highlight";
+import Image from "next/image";
+import rehypeMdxImportMedia from "rehype-mdx-import-media";
+import remarkGfm from "remark-gfm";
+import Pre from "~/app/_components/mdx/Pre";
+import Code from "~/app/_components/mdx/Code";
+import { visit } from "unist-util-visit";
+import A from "./A";
 
 function MDXRemoteWrapper({ source }: { source: string }) {
-    return (
-        <MDXRemote source={source}
-            options={{
-                mdxOptions: {
-                    rehypePlugins: [
-                        addRawCode,
-                        // @ts-expect-error: Something wrong with type definitions    
-                        rehypeHighlight,
-                        addRawToPre
-                    ],
-                    // @ts-expect-error: Something wrong with type definitions
-                    remarkPlugins: [remarkMdxImages, remarkGfm],
-                    format: 'mdx',
-                }
-            }}
-            components={({
-                h1: H1,
-                h2: H2,
-                h3: H3,
-                p: P,
-                strong: Strong,
-                blockquote: Blockquote,
-                pre: Pre,
-                ul: Ul,
-                Image: Image,
-                code: Code,
-                a: A,
-            })} />
-    )
+  return (
+    <MDXRemote
+      source={source}
+      options={{
+        mdxOptions: {
+          rehypePlugins: [
+            addRawCode,
+            // @ts-expect-error: Something wrong with type definitions
+            rehypeHighlight,
+            addRawToPre,
+          ],
+          // @ts-expect-error: Something wrong with type definitions
+          remarkPlugins: [rehypeMdxImportMedia, remarkGfm],
+          format: "mdx",
+        },
+      }}
+      components={{
+        h1: H1,
+        h2: H2,
+        h3: H3,
+        p: P,
+        strong: Strong,
+        blockquote: Blockquote,
+        pre: Pre,
+        ul: Ul,
+        Image: Image,
+        code: Code,
+        a: A,
+      }}
+    />
+  );
 }
-export default MDXRemoteWrapper
+export default MDXRemoteWrapper;
 
 const addRawCode = () => (tree: any) => {
-    visit(tree, (node) => {
-        if (node?.type === "element" && node?.tagName === "pre") {
-            const [codeEl] = node.children;
+  visit(tree, (node) => {
+    if (node?.type === "element" && node?.tagName === "pre") {
+      const [codeEl] = node.children;
 
-            if (codeEl.tagName !== "code") return;
-            node.raw = codeEl.children?.[0]!.value;
-            node.lang = ((codeEl.properties?.className?.[0]) as string).split('-')[1]
-        }
-    })
-}
+      if (codeEl.tagName !== "code") return;
+      node.raw = codeEl.children?.[0]!.value;
+      node.lang = (codeEl.properties?.className?.[0] as string).split("-")[1];
+    }
+  });
+};
 
 const addRawToPre = () => (tree: any) => {
-    visit(tree, (node) => {
-        if (node?.type === "element" && node?.tagName === "pre") {
-            node.properties.raw = node.raw;
-            node.properties.lang = node.lang;
-        }
-    })
-}
+  visit(tree, (node) => {
+    if (node?.type === "element" && node?.tagName === "pre") {
+      node.properties.raw = node.raw;
+      node.properties.lang = node.lang;
+    }
+  });
+};
